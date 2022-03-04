98760ac056938aef9407151505a8da50b549b2da
teamSuperdiv <teamSuperdiv>
2022-03-04 12:54:23 +0100

    added the book cards functionality, minor style changes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-let library = document.querySelector('.main-lib');
+let library = document.querySelector('.main-lib ul');
 let wrapper = document.querySelector('.modal-wrapper');
+let addButton = document.querySelector('#addBooktoLib');
+let inputValues = Array.from(document.querySelectorAll('input'));
 
 let myLibrary = [];
 function Book(title, author, numberOfPages, read) {
@@ -10,7 +12,7 @@ function Book(title, author, numberOfPages, read) {
     this.hasBeenread = function() {
         if (read) {
             return 'read';
-        } else return 'not read yet';
+        } else return 'not read';
     };
 }
 
@@ -21,9 +23,38 @@ Book.prototype.changeReadStatus = function () {
 }
 
 function addBooktoLibrary() {
+    myLibrary.push(new Book(inputValues[0].value, inputValues[1].value, inputValues[2].value, inputValues[3].checked));
+    displayBooks();
+    wrapper.style.visibility = 'hidden';
 }  
 
 function displayBooks() {
+    let li = document.createElement('li');
+    li.classList.add('book');
+    let bookInfo = document.createElement('div');
+    let title = document.createElement('b');
+    let restOfInfo = document.createElement('p');    
+    let buttonContainer = document.createElement('div');
+    let readUnread = document.createElement('button');
+    readUnread.classList.add('button');
+    let deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('button');
+    deleteBtn.innerText = 'delete';
+
+    
+    myLibrary.forEach((book) => {
+        title.innerText = `${book.title}`;
+        restOfInfo.innerText = `| ${book.author} | ${book.numberOfPages}`;
+        readUnread.innerText = `${book.hasBeenread()}`;
+    })
+    
+    bookInfo.appendChild(title);
+    bookInfo.appendChild(restOfInfo);
+    buttonContainer.appendChild(readUnread);
+    buttonContainer.appendChild(deleteBtn);
+    li.appendChild(bookInfo);
+    li.appendChild(buttonContainer);
+    library.appendChild(li);
 }
 
 // Toggle Modal Visibility
